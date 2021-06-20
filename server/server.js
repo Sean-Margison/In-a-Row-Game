1d7375cdce33c0b738edfd685d9246ad446b0547
Sean Margison <86201136+Sean-Margison@users.noreply.github.com>
2021-06-20 16:08:54 -0400

    CSS hover fix; heroku deploy modifications

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
+// Setup
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const port = process.env.PORT || '3001';
 const app = express();
 const server = http.createServer(app);
 const multiplayer = require('./multiplayer');
 
-// Static serve
-app.use(express.static(path.join(__dirname, "client\public")));
+// Read env variables
+const port = process.env.PORT || '3001';
+const deployed = process.env.DEPLOYED || false;
+
+// Setup express to serve deployment build
+// Necessary for single Heroku deployment
+if(deployed) {
+	const __client = __dirname.replace("\\server", '\\client');
+	
+	app.use(express.static(path.join(__client, "build")));
+
+	// Route Request
+	app.get('/', function (req, res) {
+		res.sendFile(path.join(__client, "build", "index.html"));
+	});
+}
 
 // Start 
 server.listen(port, () => console.log(`Server running on port ${port}`));
